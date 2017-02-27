833f2d92d2cda6a6b5f9bac6efcb13ac0e5a0429
Weilun Chen <warren@Weiluns-MacBook-Pro.local>
2017-02-26 22:54:42 -0800

    Finished Youtube Search Demo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -15,7 +16,11 @@ class App extends Component{
             videos: [],
             selectedVideo: null
         };
-        YTSearch({key: API_KEY, term: 'pink floyd'}, (results) => {
+        this.videoSearch('pink floyd');
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (results) => {
             this.setState({
                 videos: results,
                 selectedVideo: results[0]
@@ -23,9 +28,11 @@ class App extends Component{
         });
     }
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
         return (
             <div className="row">
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={(selectedVideo) => this.setState({selectedVideo: selectedVideo})}
